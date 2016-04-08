bd078a26898aae900b34ce901d2256ab53801bab
Julian Sauer <julian_sauer@gmx.net>
2016-04-08 16:13:02 +0200

    Saved the reference arrow as a clas variable.

--- src/main/java/de/gmx/endermansend/arrowMessages/items/ItemHandler.java
+++ src/main/java/de/gmx/endermansend/arrowMessages/items/ItemHandler.java
@@ -8,19 +8,34 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 public class ItemHandler {
 
+    private ItemStack referenceArrow;
+
+    private ItemMeta referenceArrowMeta;
+
+    public ItemHandler() {
+        referenceArrow = new ItemStack(Material.ARROW);
+        referenceArrowMeta = referenceArrow.getItemMeta();
+        referenceArrowMeta.setDisplayName("Arrow Message");
+        referenceArrow.setItemMeta(referenceArrowMeta);
+    }
+
     /**
      * Creates and adds a new arrow item that can be crafted with an arrow and a written book.
      */
     public void setUpArrowMessageItem() {
-        ItemStack arrow = new ItemStack(Material.ARROW);
-        ItemMeta meta = arrow.getItemMeta();
-        meta.setDisplayName("Arrow Message");
-        arrow.setItemMeta(meta);
 
-        ShapelessRecipe recipe = new ShapelessRecipe(arrow);
+        ShapelessRecipe recipe = new ShapelessRecipe(referenceArrow);
         recipe.addIngredient(Material.ARROW);
         recipe.addIngredient(Material.WRITTEN_BOOK);
         Bukkit.addRecipe(recipe);
+    }
+
+    public ItemStack getReferenceArrow() {
+        return referenceArrow;
+    }
+
+    public ItemMeta getReferenceArrowMeta() {
+        return referenceArrowMeta;
     }
 
 }
