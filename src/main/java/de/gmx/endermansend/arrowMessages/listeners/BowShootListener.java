9e6c0d51a99e664fca0d4f57e85504b5bbd1cc56
Julian Sauer <julian_sauer@gmx.net>
2016-04-15 22:17:03 +0200

    Fixed #2

--- src/main/java/de/gmx/endermansend/arrowMessages/listeners/BowShootListener.java
+++ src/main/java/de/gmx/endermansend/arrowMessages/listeners/BowShootListener.java
@@ -10,6 +10,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.metadata.FixedMetadataValue;
 
@@ -45,29 +46,36 @@ public class BowShootListener implements Listener {
 
     private ItemStack getShotArrow(Player player) {
 
-        Inventory inventory = player.getInventory();
-        ItemStack arrow;
+        PlayerInventory inventory = player.getInventory();
+        ItemStack arrow = inventory.getItemInOffHand();
+        if(isArrow(arrow))
+            return arrow;
 
         for (int i = 0; i < inventory.getSize(); i++) {
             arrow = inventory.getItem(i);
-            if (arrow == null)
-                continue;
-            else if (arrow.getType().equals(Material.ARROW)) {
+            if (isArrow(arrow))
                 return arrow;
-            }
         }
 
         return null;
     }
 
+    private boolean isArrow(ItemStack arrow) {
+        if (arrow == null)
+            return false;
+        return arrow.getType().equals(Material.ARROW)
+                || arrow.getType().equals(Material.SPECTRAL_ARROW)
+                || arrow.getType().equals(Material.TIPPED_ARROW);
+    }
+
     private boolean isArrowMessage(ItemMeta arrowMeta) {
         List<String> lore = arrowMeta.getLore();
-        if(lore == null)
+        if (lore == null)
             return false;
-        if(lore.isEmpty())
+        if (lore.isEmpty())
             return false;
-        for(String s : lore) {
-            if(!s.equals(""))
+        for (String s : lore) {
+            if (!s.equals(""))
                 return true;
         }
         return false;
